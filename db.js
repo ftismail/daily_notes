800da10d27f2931e2c53226cf5f2604904759876
ftismail <65233652+ftismail@users.noreply.github.com>
2022-03-29 22:03:42 +0200

    Update db.js

--- db.js
+++ db.js
@@ -5,5 +5,5 @@ const connect_string = process.env.CONNECT_STRING
 mongodb.connect(connect_string,{useNewUrlParser:true},function(err,client){
     module.exports = client
     const app = require('./app')
-    app.listen(process.env.PORT, () => console.log(`db.donnected`))
+    app.listen(process.env.PORT||3000, () => console.log(`db.donnected`))
 })
