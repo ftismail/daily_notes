f671dd58e8a4a5863de8282ba1de98382969caee
ftismail <65233652+ftismail@users.noreply.github.com>
2022-03-29 23:56:39 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongodb = require("mongodb").MongoClient;
 require('dotenv').config()
-const connect_string = process.env.CONNECT_STRING
+const connect_string = 'mongodb://ismail:loocatortor@cluster0-shard-00-00.ickzv.mongodb.net:27017,cluster0-shard-00-01.ickzv.mongodb.net:27017,cluster0-shard-00-02.ickzv.mongodb.net:27017/dailyNote?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongodb.connect(connect_string,{useNewUrlParser:true},function(err,client){
     module.exports = client
     const app = require('./app')
-    app.listen(process.env.PORT||3000, () => console.log(`db.donnected`))
+    app.listen(5000, () => console.log(`db.donnected`))
 })
